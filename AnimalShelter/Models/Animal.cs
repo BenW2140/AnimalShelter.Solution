87f08c46b9c02072fce3ad636f779ba82cd949f9
Ben White <bwhite2140@outlook.com>
2020-08-21 11:30:56 -0700

    add validation

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelter.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
+    [StringLength(30)]
     public string Name { get; set; }
+    [Required]
     public int Age { get; set; }
+    [Required]
     public string Species { get; set; }
+    [Required]
     public string Gender { get; set; }
     public bool Fixed { get; set; }
   }
